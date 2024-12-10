2e1f2334efca0b504d0a2d7234f193bcea62060b
Vijay Soni <vs4vijay@gmail.com>
2024-12-10 23:50:59 +0530

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 // #include "doomgeneric.h"
 #include "../doomgeneric/doomgeneric.h"
+#include "../doomgeneric/m_argv.h"
+#include "../doomgeneric/doomgeneric.h"
 #include <M5Cardputer.h>
 #include <M5Unified.h>
 #include <M5GFX.h>
@@ -10,27 +12,70 @@ static unsigned short s_KeyQueue[KEYQUEUE_SIZE];
 static unsigned int s_KeyQueueWriteIndex = 0;
 static unsigned int s_KeyQueueReadIndex = 0;
 
-#ifdef __cplusplus
-extern "C"
+static unsigned char convertToDoomKey(unsigned int key)
 {
-#endif
-
-  void doomgeneric_Create(int argc, char **argv);
-  void doomgeneric_Tick();
-
-#ifdef __cplusplus
+  switch (key)
+  {
+  case M5Cardputer::Key::Enter:
+    return KEY_ENTER;
+  case M5Cardputer::Key::Escape:
+    return KEY_ESCAPE;
+  case M5Cardputer::Key::Left:
+    return KEY_LEFTARROW;
+  case M5Cardputer::Key::Right:
+    return KEY_RIGHTARROW;
+  case M5Cardputer::Key::Up:
+    return KEY_UPARROW;
+  case M5Cardputer::Key::Down:
+    return KEY_DOWNARROW;
+  case M5Cardputer::Key::Ctrl:
+    return KEY_FIRE;
+  case M5Cardputer::Key::Space:
+    return KEY_USE;
+  case M5Cardputer::Key::Shift:
+    return KEY_RSHIFT;
+  default:
+    return tolower(key);
+  }
 }
-#endif
+
+static void addKeyToQueue(int pressed, unsigned int keyCode)
+{
+  unsigned char key = convertToDoomKey(keyCode);
+  unsigned short keyData = (pressed << 8) | key;
+
+  s_KeyQueue[s_KeyQueueWriteIndex] = keyData;
+  s_KeyQueueWriteIndex++;
+  s_KeyQueueWriteIndex %= KEYQUEUE_SIZE;
+}
+
+static void handleKeyInput()
+{
+  M5Cardputer::KeyEvent e;
+  while (M5Cardputer::pollEvent(e))
+  {
+    if (e.type == M5Cardputer::KeyEvent::KeyDown)
+    {
+      addKeyToQueue(1, e.key);
+    }
+    else if (e.type == M5Cardputer::KeyEvent::KeyUp)
+    {
+      addKeyToQueue(0, e.key);
+    }
+  }
+}
 
 void DG_Init()
 {
   M5.begin();
-  // Initialize M5Stack Cardputer
+  M5Cardputer::begin();
+  M5GFX::begin();
 }
 
 void DG_DrawFrame()
 {
-  M5.Lcd.drawBitmap(0, 0, DOOMGENERIC_RESX, DOOMGENERIC_RESY, (uint16_t *)DG_ScreenBuffer);
+  M5GFX::drawBitmap(0, 0, DOOMGENERIC_RESX, DOOMGENERIC_RESY, DG_ScreenBuffer);
+  handleKeyInput();
 }
 
 void DG_SleepMs(uint32_t ms)
@@ -43,34 +88,10 @@ uint32_t DG_GetTicksMs()
   return millis();
 }
 
-// int DG_GetKey(int* pressed, unsigned char* key) {
-//   *pressed = 0;
-//   *key = 0;
-
-//   if(M5.BtnA.wasPressed()) {
-//     *pressed = 1;
-//     *key = KEY_ENTER;
-//     return 1;
-//   }
-//   else if(M5.BtnB.wasPressed()) {
-//     *pressed = 1;
-//     *key = KEY_FIRE;
-//     return 1;
-//   }
-//   else if(M5.BtnC.wasPressed()) {
-//     *pressed = 1;
-//     *key = KEY_USE;
-//     return 1;
-//   }
-
-//   return 0;
-// }
-
 int DG_GetKey(int *pressed, unsigned char *doomKey)
 {
   if (s_KeyQueueReadIndex == s_KeyQueueWriteIndex)
   {
-    // key queue is empty
     return 0;
   }
   else
@@ -84,28 +105,21 @@ int DG_GetKey(int *pressed, unsigned char *doomKey)
 
     return 1;
   }
+}
+
+void DG_SetWindowTitle(const char *title)
+{
+  // No window title to set for M5Stack Cardputer
+}
+
+int main(int argc, char **argv)
+{
+  doomgeneric_Create(argc, argv);
+
+  for (;;)
+  {
+    doomgeneric_Tick();
+  }
 
   return 0;
 }
-
-void setup()
-{
-  doomgeneric_Create(0, nullptr);
-}
-
-void loop()
-{
-  doomgeneric_Tick();
-}
-
-// int main(int argc, char **argv)
-// {
-//   doomgeneric_Create(argc, argv);
-
-//   while (1)
-//   {
-//     doomgeneric_Tick();
-//   }
-
-//   return 0;
-// }
